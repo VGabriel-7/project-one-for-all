3376b2d4ef318b9c8691578d9bd01c83a0d49344
Gabriel Vinícius <viniciusgsa99@gmail.com>
2022-09-27 22:00:58 -0300

    Requisito 4 concluído. Aleluia! Exibindo as pessoas usuárias que estão ativas a partir do ano de 2021, se baseando na data mais recente no histórico de reprodução.

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT
+	U.`name` AS 'usuario',
+    IF(MAX(YEAR(Ph.release_date_of)) >= 2021,
+		'Usuário ativo', 'Usuário inativo') AS 'status_usuario'
+FROM SpotifyClone.Users AS `U`
+INNER JOIN SpotifyClone.Playback_History AS `Ph`
+ON Ph.id_Song = U.id_User
+GROUP BY U.`name`
+ORDER BY U.`name`;
