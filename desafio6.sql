9d6f53d5e16213a323fc6dc98e5563d3b0dcd266
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-02-24 10:56:15 -0300

    All changes

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT 
-  FORMAT(ROUND(MIN(Pl.price)), 2) AS `faturamento_minimo`,
-	FORMAT(ROUND(MAX(Pl.price), 2), 2) AS `faturamento_maximo`,
-  FORMAT(ROUND(AVG(Pl.price), 2), 2) AS `faturamento_medio`,
-  FORMAT(ROUND(SUM(Pl.price), 2), 2) AS `faturamento_total`
+  FORMAT(MIN(Pl.price), 2) AS `faturamento_minimo`,
+	FORMAT(MAX(Pl.price), 2) AS `faturamento_maximo`,
+  FORMAT(AVG(Pl.price), 2) AS `faturamento_medio`,
+  FORMAT(SUM(Pl.price), 2) AS `faturamento_total`
 FROM SpotifyClone.Plans AS `Pl`
 INNER JOIN SpotifyClone.Users AS `U`
 ON `U`.`id_Plan` = `Pl`.`id_Plan`;
